4d6e97303105d9d838eca164dfd03574397ac879
Alex Vasconcelos <lekobarros@outlook.com>
2022-11-22 16:11:13 -0300

    test

---
+++ .storybook/main.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'src': path.resolve(__dirname, "../src"),
+    };
+
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      loader: require.resolve("@svgr/webpack")
+    });
+
+    return config;
+  }
+}
---
+++ components/Button/Listen/ButtonListen.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Button from './index';
+
+export default {
+  title: 'Components/Button/Listen',
+  component: Button
+} as Meta;
+
+export const Default : StoryObj = {
+
+}
